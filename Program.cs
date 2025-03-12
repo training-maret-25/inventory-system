ae8a58cb8d319bdcc1994a2c422aba0ccfea0fd1
Aufal290 <138507284+Aufal290@users.noreply.github.com>
2025-03-12 17:12:43 +0700

    ini

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class UserAdd
+    {
+        private List<User> users = new List<User>();
+        private int nextId = 1;
+
+        public void AddEmployer(string username, string password)
+        {
+            foreach (var user in users)
+            {
+                if (user.Username.Equals(username, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Username udh ada");
+                    return;
+                }
+            }
+            User newUser = new User
+            {
+                Id = nextId,
+                Username = username,
+                Password = password,
+                Role = "Employer"
+            };
+            users.Add(newUser);
+            nextId++;
+
+            Console.WriteLine("Berhasil Menambahkan");
+        }
+    }
+    static void Main(string[] args)
+    {
+        UserAdd userAdd = new UserAdd();
+        while (true)
+        {
+            Console.Write("Masukkan nama Employer ");
+            string username = Console.ReadLine();
+            Console.Write("Masukkan password Employer ");
+            string password = Console.ReadLine();
+            userAdd.AddEmployer(username, password);
+        }
+    }
+}
