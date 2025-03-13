59633d07bdc769c3a289702505b0a7e8714b7f4e
Aufal290 <138507284+Aufal290@users.noreply.github.com>
2025-03-13 08:27:07 +0700

    updating user add

--- Program.cs
+++ Program.cs
@@ -3,50 +3,56 @@ using System.Collections.Generic;
 
 class Program
 {
-
-
-
-    public class User
+    static void Main()
     {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
-    }
-
-<<<<<<< HEAD
-    static void Main(string[] args)
-    {
-        UserManager.UserAdd userAdd = new UserManager.UserAdd();
+        UserManager userManager = new UserManager();
 
         while (true)
         {
-            Console.Write("Enter Employer Username: ");
-            string username = Console.ReadLine();
-            Console.Write("Enter Employer Password: ");
-            string password = Console.ReadLine();
-            userAdd.AddEmployer(username, password);
-=======
-        Console.Write("Masukkan username: ");
-        string username = Console.ReadLine() ?? "";
+            Console.Clear();
+            Console.WriteLine("=== Menu ===");
+            Console.WriteLine("1. Tambah User");
+            Console.WriteLine("2. Login");
+            Console.WriteLine("3. Keluar");
+            Console.Write("Pilih opsi (1-3): ");
+            string option = Console.ReadLine() ?? "";
 
-        Console.Write("Masukkan password: ");
-        string password = Console.ReadLine() ?? "";
+            if (option == "1")
+            {
+                userManager.UserAdd();
+            }
+            else if (option == "2")
+            {
+                Console.Write("Masukkan username: ");
+                string username = Console.ReadLine() ?? "";
 
+                Console.Write("Masukkan password: ");
+                string password = Console.ReadLine() ?? "";
 
-        bool isSuccess = userManager.Login(username, password);
+                bool isSuccess = userManager.Login(username, password);
 
-        if (!isSuccess)
-        {
-            Console.WriteLine("Login gagal.");
->>>>>>> 4d356704247bda9272a90665a90689dbf0879f0b
+                if (!isSuccess)
+                {
+                    Console.WriteLine("Login gagal. Tekan tombol apapun untuk kembali ke menu...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Login sukses. Tekan tombol apapun untuk keluar...");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            else if (option == "3")
+            {
+                Console.WriteLine("Keluar dari program...");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Pilihan tidak valid. Tekan tombol apapun untuk kembali...");
+                Console.ReadKey();
+            }
         }
-        else
-        {
-            Console.WriteLine("Login sukses.");
-        }
-
-        Console.WriteLine("Tekan tombol apapun untuk keluar...");
-        Console.ReadKey();
     }
 }
--- obj/Debug/net8.0/inventory.AssemblyInfo.cs
+++ obj/Debug/net8.0/inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea687d78165313b11ec9eb0dd757537ff8b5feb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705a38c74af0bfab71c91f808bb794b79d59377a")]
 [assembly: System.Reflection.AssemblyProductAttribute("inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
