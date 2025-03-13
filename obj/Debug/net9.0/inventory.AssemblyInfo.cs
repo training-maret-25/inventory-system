5ade8a03033510705ac8a02d9e968d409ca71a9a
Aufal290 <138507284+Aufal290@users.noreply.github.com>
2025-03-13 08:35:38 +0700

    Simpan perubahan lokal sebelum pull

--- obj/Debug/net9.0/inventory.AssemblyInfo.cs
+++ obj/Debug/net9.0/inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59633d07bdc769c3a289702505b0a7e8714b7f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0b902fbcbb2f4c1304fb6837cb2e0a488259dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
