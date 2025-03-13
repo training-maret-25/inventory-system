8d895eb7711a5c0aed78999fdf5da57790c39602
Aufal290 <138507284+Aufal290@users.noreply.github.com>
2025-03-14 01:49:44 +0700

    Feat: menambahkan fiutr

--- obj/Debug/net9.0/inventory.AssemblyInfo.cs
+++ obj/Debug/net9.0/inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd4d89de13b0aa03766f9789d26db76146061dbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb78251e7535c2aeee18bed76444b307b36b40a")]
 [assembly: System.Reflection.AssemblyProductAttribute("inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
