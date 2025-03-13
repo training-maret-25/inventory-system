09e452b96092251bf9ce5eb83882fb9e7adb51ca
Muchsinnn <muchsinikh123@gmail.com>
2025-03-13 12:11:21 +0700

    feat: fitur edit

--- obj/Debug/net9.0/inventory.AssemblyInfo.cs
+++ obj/Debug/net9.0/inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f98af1ee4ba9c65bcabd041d793ca2725650721")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f850b9e1a7de309ae18f4f9bc82f9334098537")]
 [assembly: System.Reflection.AssemblyProductAttribute("inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
