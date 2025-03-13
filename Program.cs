4f4b596460a2c087dcbd2b2840934b3acd5b47ba
Muchsinnn <muchsinikh123@gmail.com>
2025-03-13 08:40:53 +0700

    13-03

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using InventorySystem.Services;
+
+namespace InventorySystem
+{
+    class Program
+    {
+        static void Main()
+        {
+            UserManager userManager = new UserManager();
+
+            Console.WriteLine("=== Aplikasi Manajemen User ===\n");
+            userManager.DisplayUsers();
+
+            Console.Write("\nMasukkan ID User yang ingin dihapus: ");
+            string? inputId = Console.ReadLine();
+
+            if (int.TryParse(inputId, out int userId))
+            {
+                Console.Write("Masukkan nama admin yang melakukan penghapusan: ");
+                string? adminName = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(adminName))
+                {
+                    userManager.DeleteUserById(userId, adminName);
+                }
+                else
+                {
+                    Console.WriteLine("Nama admin tidak boleh kosong.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ID tidak valid. Harus berupa angka.");
+            }
+
+            Console.WriteLine("Daftar User setelah update:");
+            userManager.DisplayUsers();
+        }
+    }
+}
