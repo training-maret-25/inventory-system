1ea1861e029456c6df6f73e0b4798720f023d791
conen17 <darkconen17@gmail.com>
2025-03-14 14:13:54 +0700

    simpan ulang lokal sebelum perubahan

--- obj/Debug/net8.0/inventory.AssemblyInfo.cs
+++ obj/Debug/net8.0/inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf3d7a26c29e1cbe030897869b18e356410421db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709ff78e543d33e6f48c7f46de3a5113c5a23877")]
 [assembly: System.Reflection.AssemblyProductAttribute("inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
