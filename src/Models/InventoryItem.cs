6d898597cd39389d3ef22c146107982f4d13736b
Aufal290 <138507284+Aufal290@users.noreply.github.com>
2025-03-14 09:54:17 +0700

    feat: updatin program

--- src/Models/InventoryItem.cs
+++ src/Models/InventoryItem.cs
@@ -6,7 +6,7 @@ namespace InventorySystem.Models
         public string Nama { get; set; }
         public string Kategori { get; set; }
         public int Stok { get; set; }
-        public int BatasMinimum { get; set; }
+        public int BatasMinimum { get; set; } = 10;
         public int JumlahRestok { get; set; }
     }
 }
